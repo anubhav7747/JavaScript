5a38a448d1705b30cd655dd6e2562596a84b85a1
anubhav7747 <anubhavtiwari007005@gmail.com>
2023-12-20 16:16:39 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Please");    // we are using nodejs, not browser
+
+console.log(3 + 3);
+console.log("Please");
+
+// could readability should be high
+
+
+let name = "anubhav";
+let age = 22;
+let isLoggedIn = false;
+let state = null;
+
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined => abhi value assign nahi huyi hai
+// symbol => unique
+
+
+// object
+// console.log(typeof "snubha")
+// console.log(typeof age)
+// console.log(typeof null)    // null is object
+console.log(typeof undefined)
