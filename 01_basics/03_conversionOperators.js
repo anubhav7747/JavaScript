09988422d62d1ece76762331f649c834dc03dc48
anubhav7747 <anubhavtiwari007005@gmail.com>
2023-12-23 21:07:27 +0530

    conversion of datatype

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,77 @@
+// let score = 33
+// console.log(typeof score);  // number
+// console.log(typeof (score));    // number
+
+// console.log();
+
+
+// let scor = "33"
+// console.log(typeof scor);   // string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);  // number
+
+// console.log();
+
+
+// let sco = "33abc"
+// let valueInNumbe = Number(sco)
+// console.log(typeof valueInNumbe);   // number
+// console.log(valueInNumbe);  // NaN
+
+// console.log();
+
+
+// let sc = null
+// let valueInNumb = Number(sc)
+// console.log(typeof valueInNumb);   // number
+// console.log(valueInNumb);  // 0
+
+// console.log();
+
+
+// let s = true;
+// let valueInNum = Number(s)
+// console.log(typeof valueInNum);   // number
+// console.log(valueInNum);  // 1
+
+// console.log();
+
+
+// let str1 = "true";
+// let valueInNu = Number(str1);
+// console.log(typeof valueInNu);   // number
+// console.log(valueInNu);  // NaN
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); // true
+
+
+// let isLoggedIn = "";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); // false
+
+
+// let isLoggedIn = "happy";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); // true
+
+
+// 1 => true; 0 => false
+// "" => false
+// "happy" => true
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);  // 33
+console.log(typeof stringNumber);   // string
