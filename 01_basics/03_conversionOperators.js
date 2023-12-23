515516b2a1ff3c59653cffd0915fd31f50196a05
anubhav7747 <anubhavtiwari007005@gmail.com>
2023-12-23 22:23:41 +0530

    added Operations

--- 01_basics/03_conversionOperators.js
+++ 01_basics/03_conversionOperators.js
@@ -71,7 +71,53 @@
 // "happy" => true
 
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber);  // 33
-console.log(typeof stringNumber);   // string
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);  // 33
+// console.log(typeof stringNumber);   // string
+
+
+// ********************* Operations *********************
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);  // -3
+
+
+// console.log(2+2);   // 4
+// console.log(2-2);   // 0
+// console.log(2*2);   // 4
+// console.log(2**3);  // 8
+// console.log(2/3);   // 0.666666666
+// console.log(2%3);   // 2
+
+// let str1 = "hello"
+// let str2 = " world"
+// let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2);   // 12
+// console.log(1 + "2");   // 12
+// console.log("1" + "2"); // 12
+// console.log("1" + 2 + 2);   // 122
+// console.log(1 + 2 + "2");   // 32
+
+// console.log(3 + 4 * 5 % 3); // don't use
+
+// console.log (true);
+// console.log (+true);    // 1
+// console.log (true+);    // error
+
+// console.log(+"");   // 0
+
+// don't do
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2
+// console.log(num1, num2, num3);
+
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
