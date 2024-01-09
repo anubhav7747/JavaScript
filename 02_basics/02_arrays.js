2d8bb7022c5d9914f194c8fcd5c526ab3e856232
anubhav7747 <anubhavtiwari007005@gmail.com>
2024-01-09 20:07:01 +0530

    Update 02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -21,3 +21,10 @@ const dcHeroes = [ "superman", "flash", "batman" ];
 // console.log(realAnotherArray);  // [ 1, 2, 3, 4,  5, 6, 7, 8, 9, 10, 11 ]
 
 
+console.log(Array.isArray("Anubhav"))   // false
+console.log(Array.from("Anubhav"))  // ['A', 'n', 'u', 'b', 'h','a', 'v']
+console.log(Array.from({name: "Anubhav"}))  // []
+
+
+let srcore1 = 100, srcore2=200, score3 = 300;
+console.log(Array.of(srcore1, srcore2, score3)); // [100, 200, 300]
