3437fea80d6cb3a83938eb5fce8d2a53e6b4370a
anubhav7747 <anubhavtiwari007005@gmail.com>
2024-02-27 23:57:45 +0530

    Create 02_scopes.js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,16 @@
+// let a = 10;
+// const b = 10;
+// var c = 10;
+
+// console.log(a, b, c);
+
+
+if (true) {
+    let a = 10;
+    const b = 10;
+    var c = 10;
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
