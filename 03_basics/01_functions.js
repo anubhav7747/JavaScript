c71c85eb6746957ac42be111a07c37034c7e4cd5
anubhav7747 <anubhavtiwari007005@gmail.com>
2024-02-27 23:46:16 +0530

    Update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -54,4 +54,61 @@ function loginUserMessage(username="Mandalorian") {
     return `${username} just logged in`;
 }
 
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+// console.log(loginUserMessage('anubhav'));
+
+
+
+// function calculateCartPrice (num1) {
+//     return num1;
+// }
+
+
+// '...' acts as both rest and spread operator. It defers in name when and where used it is used.
+
+
+// function calculateCartPrice (...num1) {
+//     return num1;    // [ 200, 400, 600 ]
+// }
+
+// console.log(calculateCartPrice(200, 400, 600, 700));
+
+
+// function calculateCartPrice (val1, val2, ...num1) {
+//     return num1;    // [ 600, 700 ]
+// }
+
+// console.log(calculateCartPrice(200, 400, 600, 700));
+
+
+const user = {
+    username: 'anubhav',
+    price: 100,
+};
+
+const user1 = {
+    username: 'anubhav',
+    prices: 100,
+};
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user);
+// handleObject(user1);
+// handleObject({
+//     username: 'anubhav',
+//     price: 200,
+// });
+
+
+const myNewArray = [ 200, 300, 400, 500 ];
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([ 200, 500, 700 ]));
