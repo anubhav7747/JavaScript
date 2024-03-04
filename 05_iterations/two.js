e8908b1b3c1377abc507c78b25d35d407dd47084
anubhav7747 <anubhavtiwari007005@gmail.com>
2024-03-05 00:31:04 +0530

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,27 @@
+// while
+
+// let index = 0;
+// while (index < 10) {
+//     console.log(index);
+//     index += 2;
+// }
+
+
+
+// let arr = ['BMW', 'MG Hector', 'Tata'];
+
+// let index = 0
+// while (index < arr.length) {
+//     console.log(arr[index]);
+//     index++;
+// }
+
+
+
+// let score = 1;
+// let score = 11; // it prints 11 even though it is greater than 10.
+
+// do {
+//     console.log(`Score: ${score}`);
+//     score++;
+// } while (score <= 10);
