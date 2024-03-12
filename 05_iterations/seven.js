5250da29842b096fb8115a43ca01f986feebfb17
anubhav7747 <anubhavtiwari007005@gmail.com>
2024-03-12 17:43:37 +0530

    map

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,4 +1,13 @@
-const myNum = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const newMyNum = myNum.map( (num) => num + 10 );
-console.log(newMyNum);
+const newMyNum = myNum.map((num) => num + 10);
+// console.log(newMyNum);
+
+// const newNums = myNum.map().map().filter;
+
+const newNums = myNum
+  .map((num) => num * 10)
+  .map((num) => num + 10)
+  .filter((num) => num <= 80)
+  .filter((num) => num >= 40);
+console.log(newNums);
